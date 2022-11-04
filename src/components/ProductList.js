c30ada0a7afb7baeaa2e3e66565e4580952408c0
ReynaEstrada <iscsoftware384@gmai.com>
2022-11-03 21:01:17 -0600

    Css_Tejuinoshop

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,33 @@
 import {useEffect, useState} from 'react';
 import Product from './Product';
-import React from 'react'
 
-function ProductList() {
 
+function NavPage(props){
+  return (
+  <header className='flex justify-between'>
+      <p>Page {props.page}</p>
+      <button className='px-4 rounded-t-lg  bg-purple-800 text-black border-black'
+      onClick={() => props.setPage(props.page +1)}>  
+        Page {props.page + 1}
+      </button>
+  </header>
+  )
+}
+function ProductList(props) {
     const [ProductTec, setonlyProducts] = useState([])
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         async function fetchProductos() {
-        const response = await  fetch ('https://dummyjson.com/products');
+        const response = await  fetch(`https://dummyjson.com/products?page=${page}`);
         const data = await response.json();
         setonlyProducts(data.products);
    }
    fetchProductos();
-    }, []);
+    }, [page]);
   return (
     <div className='container mx-auto px-6 bg-slate-400'>
+        <NavPage page={page} setPage={setPage}/>
       <div className='grid grid-cols-3 gap-2'>
         {ProductTec.map((product) => {
         return (
@@ -25,6 +37,7 @@ function ProductList() {
         );
         })}
       </div>
+      <NavPage page={page} setPage={setPage}/>
     </div>
   );
 }
