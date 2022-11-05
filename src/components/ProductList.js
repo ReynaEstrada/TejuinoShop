3127674e330b4b861075e39d4a31a250e2ea169b
JosMIgue <jojo_miguel20@hotmail.com>
2022-11-04 22:47:11 -0600

    Fixes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -14,29 +14,24 @@ function NavPage(props){
   )
 }
 function ProductList(props) {
-    const [ProductTec, setonlyProducts] = useState([])
-    const [page, setPage] = useState(1)
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1)
 
-    useEffect(() => {
-        async function fetchProductos() {
-        const response = await  fetch(`https://dummyjson.com/products?page=${page}`);
-        const data = await response.json();
-        setonlyProducts(data.products);
-   }
-   fetchProductos();
-    }, [page]);
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await  fetch(`https://dummyjson.com/products?page=${page}`);
+      const data = await response.json();
+      setProducts(data.products);
+    }
+    fetchProducts();
+  }, [page]);
+
   return (
     <div className='container mx-auto px-6 bg-slate-400'>
-        <NavPage page={page} setPage={setPage}/>
-      <div className='grid grid-cols-3 gap-2'>
-        {ProductTec.map((product) => {
-        return (
-          <div >
-            <Product  key={product.id} Product={product}/>
-          </div>
-        );
-        })}
-      </div>
+      <NavPage page={page} setPage={setPage}/>
+        <div className='grid lg:grid-cols-3  grid-cols-1 gap-2'> 
+          {products.map((product) => <Product  key={product.id} item={product}/>)}
+        </div>
       <NavPage page={page} setPage={setPage}/>
     </div>
   );
